a69f596f4b1b163eee1abd7896763835f415ce38
Jeremy Hamilton <jeremyhamilton035@gmail.com>
2019-07-22 19:13:24 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,8 +1,19 @@
 def my_each(words)
-  counter = 0
-  words.map do |word|
-  while counter > 0
-  return counter
+ if block_given?
+    i = 0
+ 
+    while i < array.length
+      yield(array[i])
+      i = i + 1
+    end
 end
-end
-end
+
+
+
+#   counter = 0
+#   words.map do |word|
+#   while counter > 0
+#   return counter
+# end
+# end
+# end
