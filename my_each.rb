d4ced2780e3c2380e224991613f1904f9961ad94
Jeremy Hamilton <jeremyhamilton035@gmail.com>
2019-07-22 19:26:40 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,12 +1,4 @@
-# def my_each(words)
-#   counter = 0
-#   words.each_with_index do |word|
-#   while counter > 0
 
-#   return counter
-# end
-# end
-# end
 def my_each(collection)
   i = 0
   while i < collection.length
