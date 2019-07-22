088694b367baba7223a6f0d421c781afccc40a09
Jeremy Hamilton <jeremyhamilton035@gmail.com>
2019-07-22 19:25:16 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,9 +1,9 @@
-def my_each(words)
-  counter = 0
-  words.each_with_index do |word|
-  while counter > 0
+# def my_each(words)
+#   counter = 0
+#   words.each_with_index do |word|
+#   while counter > 0
 
-  return counter
-end
-end
-end
+#   return counter
+# end
+# end
+# end
